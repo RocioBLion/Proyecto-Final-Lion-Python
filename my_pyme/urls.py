422a514c8757e6be404c146879b118b9e4f0d17a
RocioBLion <rociobelenlion@hotmail.com>
2022-11-15 22:27:00 -0300

    add form.html and list.html

--- my_pyme/urls.py
+++ my_pyme/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from cellphone.views import create_cellphones
+from computer.views import create_computers
+from accessorie.views import create_accessories
 
 urlpatterns = [
     path('admin/', admin.site.urls),
