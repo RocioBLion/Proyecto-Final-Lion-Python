e4ce81bd533bd422378a94855bf8d8cf7d21de01
German <germanht14@hotmail.com>
2022-11-17 01:58:48 -0600

    Add image to class and Create login

--- computer/models.py
+++ computer/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Computer (models.Model):
     brand = models.CharField(max_length=20)
     model =  models.CharField(max_length=20)
-    description = models.CharField(max_length=40)
+    description = models.TextField()
     price = models.IntegerField()
     image = models.ImageField(upload_to="products", null=True)
 
