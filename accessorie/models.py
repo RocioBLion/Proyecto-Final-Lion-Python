c03be315fa943f5d5235713be768b3ee1f200daa
German <germanht14@hotmail.com>
2022-11-17 01:51:08 -0600

    Add image to class and Create login

--- accessorie/models.py
+++ accessorie/models.py
@@ -5,6 +5,9 @@ class Accessorie (models.Model):
     model =  models.CharField(max_length=20)
     description = models.CharField(max_length=40)
     price = models.IntegerField()
+    image = models.ImageField(upload_to="products", null=True)
+
+
 
     def __str__(self):
         return f"{self.brand} - {self.model} - {self.description} - {self.price}"
