c166e0eb5a83f0ea9a67431f766a0efb88d0979a
German <germanht14@hotmail.com>
2022-11-21 03:44:33 -0600

    Login & logout

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,37 @@
-from django.urls import path 
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from django.urls import reverse_lazy
 
-from home import views 
+from home import views
 
 app_name = "home"
 urlpatterns = [
     path("", view=views.index, name="index"),
+    path("register/", view=views.register, name="user-register"),
+    path('register/update/', view=views.user_update, name='user-update'),
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='registration/change-password.html',
+            success_url=reverse_lazy("home:password-change-done")
+        ),
+        name="password-change"
+    ),
+    path(
+        'password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='registration/change-password-done.html'
+        ),
+        name="password-change-done"
+    ),
 ]
+
+
+
+
+
+
+
+
+
 
