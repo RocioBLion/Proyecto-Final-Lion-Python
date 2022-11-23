921f3cff1306a36698bfc7d93e6bf4ee68eca2a5
Andrea Regueira <andrearegueira324+person@gmail.com>
2022-11-22 23:16:36 -0300

    Cambios

--- computer/models.py
+++ computer/models.py
@@ -1,11 +1,52 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
+from ckeditor.fields import RichTextField
+
+
+
+class Computer(models.Model):
+    brand= models.CharField(max_length=40, null=False, blank=False)
+    model = models.IntegerField(null=False, blank=False)
+    description = RichTextField(null=True, blank=True)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='computer', null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.ManyToManyField(
+        User, through="Comment", related_name="comments_owned"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (
+            "brand",
+            "model",
+        )
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Computer: {self.brand} - {self.model} - {self.description} - {self.price}"
+
+
+class Comment(models.Model):
+    text = models.TextField(
+        validators=[
+            MinLengthValidator(10, "El comentario debe ser mayor de 10 caracteres")
+        ]
+    )
+    computer = models.ForeignKey(Computer, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class Computer (models.Model):
     brand = models.CharField(max_length=20)
     model =  models.CharField(max_length=20)
     description = models.TextField()
     price = models.IntegerField()
-    image = models.ImageField(upload_to="products", null=True)
+    image = models.ImageField(upload_to='computer', null=True, blank=True)
 
     def __str__(self):
         return f"{self.brand} - {self.model} - {self.description} - {self.price}"
--- computer/urls.py
+++ computer/urls.py
@@ -4,8 +4,11 @@ from computer import views
 
 app_name = "computer"
 urlpatterns = [
-    path("computers", view=views.computers, name="computer-list"),
-    path("computers", view=views.computers, name="computer-list"),
-    path("computer/add", view=views.create_computers, name="computer-add"),
-    
+    path("courses/", views.ComputerListView.as_view(), name="course-list"),
+    path("course/add/", views.ComputerCreateView.as_view(), name="course-add"),
+    path("course/<int:pk>/detail/", views.ComputerDetailView.as_view(), name="course-detail"),
+    path("course/<int:pk>/update/", views.ComputerUpdateView.as_view(), name="course-update"),
+    path("course/<int:pk>/delete/", views.ComputerDeleteView.as_view(), name="course-delete"),
+    path("comment/<int:pk>/add/", views.CommentCreateView.as_view(), name="comment-create"),
+    path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
 ]
