c291b0455bf852673f2e62fcd354890ee2b53d7e
RocioBLion <rociobelenlion@hotmail.com>
2022-11-22 23:19:33 -0300

    add change

--- computer/models.py
+++ computer/models.py
@@ -5,7 +5,7 @@ class Computer (models.Model):
     model =  models.CharField(max_length=20)
     description = models.TextField()
     price = models.IntegerField()
-    image = models.ImageField(upload_to="products", null=True)
+    image = models.ImageField(upload_to='products', null=True)
 
     def __str__(self):
         return f"{self.brand} - {self.model} - {self.description} - {self.price}"
