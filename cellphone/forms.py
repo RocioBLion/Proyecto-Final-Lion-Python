2c94a1de21d4c574d3a8d2745f28f335fca62fb3
Andrea Regueira <andrearegueira324+person@gmail.com>
2022-11-24 20:49:36 -0300

    Update

--- cellphone/forms.py
+++ cellphone/forms.py
@@ -1,50 +1,65 @@
 from django import forms
-
+from ckeditor.widgets import CKEditorWidget
 
 from cellphone.models import Cellphone
 
-class CellphoneForm(forms.Form):
+class CellphoneForm(forms.ModelForm):
     brand = forms.CharField(
-        label="Brand:",
+        label="Cellphone brand",
+        max_length=40,
         required=False,
         widget=forms.TextInput(
             attrs={
                 "class": "cellphone-brand",
-                "placeholder": "Brand",
+                "placeholder": "Cellphone brand",
                 "required": "True",
             }
         ),
     )
-    model = forms.CharField(
+
+    model = forms.IntegerField(
         label="Model:",
         required=False,
         widget=forms.TextInput(
             attrs={
                 "class": "cellphone-model",
-                "placeholder": "Model",
+                "placeholder": "Cellphone model",
                 "required": "True",
             }
         ),
-    )    
+    )
+
     description = forms.CharField(
         label="Description:",
         required=False,
-        widget=forms.TextInput(
-            attrs={
-                "class": "cellphone-description",
-                "placeholder": "Characteristics",
-                "required": "True",
-            }
-        ),
-    )    
-    price = forms.IntegerField(
-        label="Price:",
+        widget=CKEditorWidget(),
+    )
+
+    image = forms.ImageField()
+
+    class Meta:
+        model = Cellphone
+        fields = ["brand", "model", "description", "price"]
+
+
+class CommentForm(forms.Form):
+    comment_text = forms.CharField(
+        label="",
         required=False,
-        widget=forms.TextInput(
+        max_length=500,
+        min_length=10,
+        strip=True,
+        widget=forms.Textarea(
             attrs={
-                "class": "cellphone-price",
-                "placeholder": "Value",
+                "class": "comment-text",
+                "placeholder": "Enter a comment...",
                 "required": "True",
+                "max_length": 500,
+                "min_length": 10,
+                "rows": 2,
+                "cols": 10,
+                "style":"min-width: 100%",
             }
         ),
-    )        
+    )
+       
--- cellphone/models.py
+++ cellphone/models.py
@@ -1,11 +1,42 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
+from ckeditor.fields import RichTextField
 
-class Cellphone (models.Model):
-    brand = models.CharField(max_length=20)
-    model = models.CharField(max_length=20)
-    description = models.TextField()
+
+
+class Cellphone(models.Model):
+    brand= models.CharField(max_length=40, null=False, blank=False)
+    model = models.CharField(max_length=40, null=False, blank=False)
+    description = RichTextField(null=True, blank=True)
     price = models.IntegerField()
-    image = models.ImageField(upload_to="products", null=True)
+    image = models.ImageField(upload_to='cellphone', null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.ManyToManyField(
+        User, through="Comment", related_name="comments_owned"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (
+            "brand",
+            "model",
+            "description",
+            "price",
+        )
+        ordering = ["-created_at"]
 
     def __str__(self):
-        return f"{self.brand} - {self.model} - {self.description} - {self.price}"
+        return f"Cellphone: {self.brand} - {self.model} - {self.description} - {self.price}"
+    
+class Comment(models.Model):
+    text = models.TextField(
+        validators=[
+            MinLengthValidator(10, "The comment must be longer than 10 characters")
+        ]
+    )
+    cellphone = models.ForeignKey(Cellphone, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
